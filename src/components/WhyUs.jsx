8f8580259f51fdec4434679860e8c2619286331e
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-06-14 20:52:43 -0700

    Animate the cards into a parallax effect WhyUs.jsx

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -1,56 +1,82 @@
 import Container from "./shared/Container";
-import { motion } from "motion/react";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
 
 const cards = [
   {
     title: "Transparent Pricing",
     content: "What we quote is what you pay. No upsells. No hidden fees.",
-    color: "bg-blue-50 text-blue-900",
+    color: "bg-blue-200 text-blue-900",
   },
   {
     title: "We Haul the Old Stuff",
     content:
       "We don’t just install — we take care of the mess. Old furniture and packaging? Gone.",
-    color: "bg-yellow-50 text-yellow-900",
+    color: "bg-yellow-200 text-yellow-900",
   },
   {
     title: "Local & Trusted",
     content:
       "We’re not some faceless platform. We live here, we work here, and our reviews speak for themselves.",
-    color: "bg-green-50 text-green-900",
+    color: "bg-green-200 text-green-900",
   },
   {
     title: "Expert Technicians",
     content:
       "We work with all major brands and equipment types. Trained. Insured. Reliable.",
-    color: "bg-gray-100 text-gray-800",
+    color: "bg-gray-200 text-gray-800",
+  },
+  {
+    title: "Transparent Pricing",
+    content: "What we quote is what you pay. No upsells. No hidden fees.",
+    color: "bg-blue-200 text-blue-900",
   },
 ];
 
-function Card({ title, content, color }) {
+function Card({ title, content, color, i, targetScale, range, progress }) {
+  const scale = useTransform(progress, range, [1, targetScale]);
   return (
-    <div
+    <motion.div
       className={
-        color +
-        " p-8 py-16 rounded-lg mb-8 border-2 aspect-[4/3] sticky top-[17vh] "
+        color + " p-8 py-16 rounded-lg mb-8 border-2 aspect-[4/3] sticky"
       }
+      style={{ scale, top: `calc(17vh + ${i * 20}px)` }}
     >
       <h1 className="font-bold text-xl mb-4">{title} </h1>
       <p>{content} </p>
-    </div>
+    </motion.div>
   );
 }
 
 function WhyUs() {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
   return (
-    <Container className="py-12 relative">
-      <h1 className="text-center font-space font-bold text-2xl mb-8 sticky top-[10vh] ">
-        Why Us
-      </h1>
-      <div className="pb-2">
-        {cards.map((card, index) => {
-          return <Card key={index} {...card} />;
-        })}
+    <Container className="pt-12 pb-8">
+      <div ref={container} className="relative">
+        <h1 className="text-center font-space font-bold text-2xl pb-8 sticky top-[10vh] ">
+          Why Us
+        </h1>
+        <div className="pb-1">
+          {cards.map((card, index) => {
+            const targetScale = 1 - (cards.length - index) * 0.025;
+            return (
+              <Card
+                key={index}
+                {...card}
+                i={index}
+                targetScale={targetScale}
+                progress={scrollYProgress}
+                range={[0.25 * index, 1]}
+              />
+            );
+          })}
+        </div>
       </div>
     </Container>
   );
