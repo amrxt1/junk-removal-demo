2068a91a62b8e1dd8fedeeec7cf8a36d7ec626de
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-06-16 02:22:12 -0700

    Remove redundant layer from Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,16 +6,14 @@ import { motion } from "motion/react";
 function Hero() {
   return (
     <Container className="relative">
-      <div className="absolute inset-0" />
       <div className="absolute top-10 left-10 w-40 h-40 bg-accent rounded-full filter blur-3xl opacity-30 -z-10" />
       <div className="absolute bottom-20 right-20 w-64 h-64 bg-accent rounded-full filter blur-2xl opacity-20 -z-10" />
-
       <motion.div
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.3 }}
         id="hero"
-        className="flex flex-col pt-16 pb-24 text-center gap-8"
+        className="flex flex-col pt-16 pb-24 text-center gap-8 z-10"
       >
         <div className="flex flex-col gap-4">
           <h1 className="text-3xl font-space font-bold ">
