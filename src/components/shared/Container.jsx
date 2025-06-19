63baefd18ec2a47384113efb5c5c7272db31322e
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-06-18 19:42:24 -0700

    Update Container.jsx paddings

--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -1,6 +1,9 @@
 function Container({ className = "", id = "", children }) {
   return (
-    <div className={`px-4 ${className}`} id={id}>
+    <div
+      className={`px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 ${className} `}
+      id={id}
+    >
       {children}
     </div>
   );
